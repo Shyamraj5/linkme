7d17bf076b3c2cafe18db56cfeec950c3668b8f3
ShyamRaj <shyamrajkm5@gmail.com>
2023-07-19 23:01:01 -0700

    follow fixed

--- linkme-master/user/userurl.py
+++ linkme-master/user/userurl.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("Comment/<int:cid>",addcomment,name="cmnt"),
     path("logout/",LogOut.as_view(),name='logout'),
     
-    # path("follow/<int:fid>",following,name="follow"),
+    path("follow/<int:fid>",following,name="follow"),
 
 
 ]
